9ff1093450b503b4f449e42d869bec12b3187716
Pierre-Antoine <you@example.com>
2018-06-01 16:44:05 +0200

    production

--- src/main/java/dev/sgp/controller/CollaborateurController.java
+++ src/main/java/dev/sgp/controller/CollaborateurController.java
@@ -3,6 +3,7 @@ package dev.sgp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import dev.sgp.entite.Collaborateur;
 import dev.sgp.repository.CollaborateurRepository;
 import dev.sgp.repository.DepartementRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/collaborateur")
 public class CollaborateurController {
